0c89f025bece79335be63b81822f9dfdc9cc5850
Ben Deane <ben@elbeno.com>
2024-04-12 10:45:20 -0600

    :construction_worker: Add mypy linting for python

--- test/application/test/pytest_test.py
+++ test/application/test/pytest_test.py
@@ -2,6 +2,7 @@ from hypothesis import given, strategies as st
 
 small_ints = st.integers(min_value=0, max_value=1000)
 
+
 @given(small_ints, small_ints)
 def test_addition_is_commutative(a: int, b: int):
     assert a + b == b + a
--- test/library/include/test_lib/test.hpp
+++ test/library/include/test_lib/test.hpp
@@ -3,4 +3,4 @@
 namespace test_lib {
 template <typename T>
 concept test_concept = true;
-}
+} // namespace test_lib
