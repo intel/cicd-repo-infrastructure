716bd2a804e5721b10e271fcd09034c2f3cbc50e
Ben Deane <ben@elbeno.com>
2024-05-20 11:36:02 -0600

    :sparkles: Add CPM recipe for fmtlib
    Also, protect each recipe individually.

--- cmake/cpm_recipes.cmake
+++ cmake/cpm_recipes.cmake
@@ -1,44 +1,59 @@
-if(COMMAND boost_sml_recipe)
-    return()
+if(NOT COMMAND fmt_recipe)
+    macro(fmt_recipe VERSION)
+        add_versioned_package(
+            NAME
+            fmt
+            GITHUB_REPOSITORY
+            "fmtlib/fmt"
+            GIT_TAG
+            ${VERSION}
+            OPTIONS
+            "FMT_INSTALL OFF"
+            "FMT_OS OFF")
+    endmacro()
 endif()
 
-macro(boost_sml_recipe VERSION)
-    add_versioned_package(
-        NAME
-        sml
-        GIT_TAG
-        ${VERSION}
-        GITHUB_REPOSITORY
-        boost-ext/sml
-        OPTIONS
-        "SML_BUILD_BENCHMARKS OFF"
-        "SML_BUILD_EXAMPLES OFF"
-        "SML_BUILD_TESTS OFF")
-endmacro()
+if(NOT COMMAND boost_sml_recipe)
+    macro(boost_sml_recipe VERSION)
+        add_versioned_package(
+            NAME
+            sml
+            GITHUB_REPOSITORY
+            "boost-ext/sml"
+            GIT_TAG
+            ${VERSION}
+            OPTIONS
+            "SML_BUILD_BENCHMARKS OFF"
+            "SML_BUILD_EXAMPLES OFF"
+            "SML_BUILD_TESTS OFF")
+    endmacro()
+endif()
 
-macro(mp_units_recipe VERSION)
-    add_versioned_package("gh:gsl-lite/gsl-lite@0.40.0")
-    set(gsl-lite_DIR "${gsl-lite_BINARY_DIR}")
+if(NOT COMMAND mp_units_recipe)
+    macro(mp_units_recipe VERSION)
+        add_versioned_package("gh:gsl-lite/gsl-lite@0.40.0")
+        set(gsl-lite_DIR "${gsl-lite_BINARY_DIR}")
 
-    add_versioned_package(
-        NAME
-        fmt
-        GITHUB_REPOSITORY
-        "fmtlib/fmt"
-        GIT_TAG
-        9.1.0
-        OPTIONS
-        "FMT_INSTALL ON")
-    set(fmt_DIR "${fmt_BINARY_DIR}")
+        add_versioned_package(
+            NAME
+            fmt
+            GITHUB_REPOSITORY
+            "fmtlib/fmt"
+            GIT_TAG
+            9.1.0
+            OPTIONS
+            "FMT_INSTALL ON")
+        set(fmt_DIR "${fmt_BINARY_DIR}")
 
-    add_versioned_package(
-        NAME
-        mp-units
-        GITHUB_REPOSITORY
-        "mpusz/units"
-        GIT_TAG
-        ${VERSION}
-        DOWNLOAD_ONLY
-        YES)
-    add_subdirectory("${mp-units_SOURCE_DIR}/src")
-endmacro()
+        add_versioned_package(
+            NAME
+            mp-units
+            GITHUB_REPOSITORY
+            "mpusz/units"
+            GIT_TAG
+            ${VERSION}
+            DOWNLOAD_ONLY
+            YES)
+        add_subdirectory("${mp-units_SOURCE_DIR}/src")
+    endmacro()
+endif()
