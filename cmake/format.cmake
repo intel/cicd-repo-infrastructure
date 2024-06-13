a5b920ea3e2156d4c08e6e23471eb4a71d791ce1
Ben Deane <114956961+bdeane-intel@users.noreply.github.com>
2024-06-13 08:34:57 -0600

    Revert ":sparkles: :construction_worker: Add code coverage reports for tests"

--- cmake/format.cmake
+++ cmake/format.cmake
@@ -1,3 +1,4 @@
+get_filename_component(CT_ROOT ${CMAKE_CXX_COMPILER} DIRECTORY)
 find_program(CLANG_FORMAT_PROGRAM "clang-format" HINTS ${CT_ROOT})
 if(CLANG_FORMAT_PROGRAM)
     message(STATUS "clang-format found: ${CLANG_FORMAT_PROGRAM}")
